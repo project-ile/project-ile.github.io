8f481ec8948790566d5b6eed36022f01a16cfa34
Avinal Kumar <avinal.xlvii@gmail.com>
2023-05-24 15:19:25 +0530

    Add initial structure with themes
    Signed-off-by: Avinal Kumar <avinal.xlvii@gmail.com>

---
+++ javascript/marksheet.js
@@ -0,0 +1,43 @@
+jQuery(document).ready(function ($) {
+  // Interactions
+
+  var $toggle = $("#toggle");
+  var $down = $("#down");
+  var $overlay = $("#overlay");
+  var $top = $("#top");
+  var $banner = $("#banner");
+  var $newsletter = $("#css-ebook");
+
+  $toggle.click(function () {
+    $("html").addClass("open-menu");
+  });
+
+  $down.click(function () {
+    $("html").addClass("open-navigation");
+  });
+
+  $top.click(function () {
+    $("html, body").animate(
+      {
+        scrollTop: 0,
+      },
+      500
+    );
+  });
+
+  $banner.click(function (e) {
+    e.preventDefault();
+    $("html, body").animate(
+      {
+        scrollTop: $newsletter.offset().top,
+      },
+      500
+    );
+  });
+
+  $overlay.click(function () {
+    $("html").removeClass("open-menu").removeClass("open-navigation");
+  });
+
+  $(window).load(function () {});
+});
